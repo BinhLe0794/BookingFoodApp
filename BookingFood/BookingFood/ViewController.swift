c09113264a286e0b2e3f3a1a7d25ae91deadd0c9
Binh Le <huybinh0794@gmail.com>
2022-08-21 18:57:28 +0700

    [Feat]: Intial the project structure

---
+++ BookingFood/BookingFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookingFood
+//
+//  Created by Huy Binh on 21/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
